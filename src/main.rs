cc01e7fe6cada88505222ffdbe49142279cbc79c
Ryoh Kawai <kawairyoh@gmail.com>
2021-12-28 18:05:26 +0900

    Add StructOpt base settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rushare", about = "Run a program with some namespaces unshared from the parent.")]
+struct Opt {
+    #[structopt(short, long)]
+    mount: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
