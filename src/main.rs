979bcf0f2caf4c9e8b05513aa9a9debf593e6ae4
Ryoh Kawai <kawairyoh@gmail.com>
2022-01-02 18:39:52 +0900

    Add options

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 use anyhow::Result;
-use structopt::StructOpt;
+use nix::sched::CloneFlags;
+use structopt::{clap, StructOpt};
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "rushare", about = "Run a program with some namespaces unshared from the parent.")]
+#[structopt(name = "rushare")]
+#[structopt(about = "Run a program with some namespaces unshared from the parent.")]
+#[structopt(long_version(option_env!("LONG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))))]
+#[structopt(setting(clap::AppSettings::ColoredHelp))]
 struct Opt {
-    #[structopt(short, long)]
-    mount: bool
+    #[structopt(short, long, help = "unshare mounts namespace")]
+    mount: bool,
+
+    #[structopt(short, long, help = "fork before launching <program>")]
+    fork: bool,
+
+    prog: String,
+
+    args: Vec<String>,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
+    let mut clone_flag: CloneFlags = CloneFlags::empty();
 
     if opt.mount {
-        println!("mount option true");
+        clone_flag |= CloneFlags::CLONE_NEWNS;
     }
+
+    if opt.fork {
+        println!("forked!!");
+    }
+
+    println!("{:?}, {:?}", opt, clone_flag);
 
     Ok(())
 }
