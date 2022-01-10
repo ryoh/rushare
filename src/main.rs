0377b90ec1913cc183a04bd7bff227147d5aea2d
Ryoh Kawai <kawairyoh@gmail.com>
2022-01-10 19:23:52 +0900

    Add some namespace options

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,28 @@ struct Opt {
     #[structopt(short, long, help = "unshare mounts namespace")]
     mount: bool,
 
+    #[structopt(short, long, help = "unshare UTS namespace (hostname etc)")]
+    uts: bool,
+
+    #[structopt(short, long, help = "unshare System V IPC namespace")]
+    ipc: bool,
+
+    #[structopt(short, long, help = "unshare network namespace")]
+    net: bool,
+
+    #[structopt(short, long, help = "unshare pid namespace")]
+    pid: bool,
+
+    #[structopt(short = "U", long, help = "unshare user namespace")]
+    user: bool,
+
+    #[structopt(short = "C", long, help = "unshare cgroup namespace")]
+    cgroup: bool,
+
     #[structopt(short, long, help = "fork before launching <program>")]
     fork: bool,
 
-    #[structopt(name="program")]
+    #[structopt(name="program", default_value = "")]
     prog: String,
 
     #[structopt(name="arguments")]
@@ -29,9 +47,33 @@ fn main() -> Result<()> {
     let opt = Opt::from_args();
     let mut unshare_flags: CloneFlags = CloneFlags::empty();
 
-    // parse command arguments
+    // Set unshare namespace
     if opt.mount {
         unshare_flags |= CloneFlags::CLONE_NEWNS;
+    }
+
+    if opt.uts {
+        unshare_flags |= CloneFlags::CLONE_NEWUTS;
+    }
+    
+    if opt.ipc {
+        unshare_flags |= CloneFlags::CLONE_NEWIPC;
+    }
+
+    if opt.net {
+        unshare_flags |= CloneFlags::CLONE_NEWNET;
+    }
+
+    if opt.pid {
+        unshare_flags |= CloneFlags::CLONE_NEWPID;
+    }
+
+    if opt.user {
+        unshare_flags |= CloneFlags::CLONE_NEWUSER;
+    }
+
+    if opt.cgroup {
+        unshare_flags |= CloneFlags::CLONE_NEWCGROUP;
     }
 
     if opt.fork {
@@ -40,8 +82,8 @@ fn main() -> Result<()> {
 
     // command building
     let path = match opt.prog.as_str() {
-        "" => CString::new("/bin/sh")?,
-        _ => CString::new(opt.prog)?,
+        "" => CString::new(std::env::var("SHELL").unwrap_or("/bin/sh".to_string()))?,
+        _ => CString::new(opt.prog.clone())?,
     };
 
     let mut argv: Vec<CString> = opt.args.iter()
@@ -53,9 +95,11 @@ fn main() -> Result<()> {
     // unshare and run command
     unsafe { signal(signal::SIGCHLD, signal::SigHandler::SigDfl) }?;
 
-    unshare(unshare_flags)?;
+    // unshare
+    unshare(unshare_flags).expect("unshare failed");
 
-    execvp(&path, &argv)?;
+    // exec
+    execvp(&path, &argv).expect("execvp failed");
     
 
     Ok(())
